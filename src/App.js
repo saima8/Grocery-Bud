665ed24df66deb4207eb43b6bd9841329c043294
Saima Khan <tithi888khan@gmail.com>
2021-02-07 23:41:22 +0600

    Added form and button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isediting, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''})
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello')
+  }
+  return <section className="section-center">
+            <form className='gocery-form' onSubmit={handleSubmit}></form>
+            <div className="grocery-container">
+              <List />
+              <button className='clear-btn' >clear items</button>
+            </div>
+
+  </section>
 }
 
 export default App
